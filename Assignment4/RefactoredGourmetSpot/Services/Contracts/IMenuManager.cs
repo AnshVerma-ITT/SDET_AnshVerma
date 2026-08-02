5788054a7cca797a75dff5d8e953b12b4a5f8f73
Ansh Verma <ansh.verma@intimetec.com>
2026-08-02 20:08:33 +0530

    Rempved warnings

--- Assignment4/RefactoredGourmetSpot/Services/Contracts/IMenuManager.cs
+++ Assignment4/RefactoredGourmetSpot/Services/Contracts/IMenuManager.cs
@@ -8,8 +8,8 @@ namespace GourmetSpot.Services.Contracts
 
         int GetNextMenuItemId();
         bool AddMenuItem(MenuItem menuItem, out string message);
-        MenuItem SearchMenuItemById(int menuItemId);
-        MenuItem SearchMenuItemByName(string menuItemName);
+        MenuItem? SearchMenuItemById(int menuItemId);
+        MenuItem? SearchMenuItemByName(string menuItemName);
         List<MenuItem> GetAllMenuItems();
         Dictionary<int, double> CreateRecipe();
         void AddRecipeIngredient(Dictionary<int, double> recipeIngredients, int ingredientId, double requiredQuantity);
