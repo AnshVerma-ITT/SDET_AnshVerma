17fccaf1bb47cdf15210799e27c8facff9b4d0d4
Ansh Verma <ansh.verma@intimetec.com>
2026-09-21 12:08:20 +0530

    Testing commit for practice 11

--- Assignment11/HRMS Portal Automation/Configuration/TestTags.cs
+++ Assignment11/HRMS Portal Automation/Configuration/TestTags.cs
@@ -2,6 +2,11 @@ namespace HRIntimeAutomation.Configuration;
 
 public static class TestTags
 {
+    public const string Smoke = "smoke";
+    public const string Regression = "regression";
+    public const string ReadOnly = "readOnly";
+    public const string DataMutation = "dataMutation";
+
     public const string Leave = "leave";
     public const string LeaveCorrection = "leaveCorrection";
     public const string NoBrowser = "noBrowser";
